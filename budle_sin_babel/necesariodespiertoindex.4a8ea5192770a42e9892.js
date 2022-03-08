fa7750a7b2c0da92785584c949dd557f6e9b75b7
Ángel Higueros <45275116+angelhigueros@users.noreply.github.com>
2022-03-08 12:06:18 -0600

    Add files via upload

---
+++ budle_sin_babel/necesariodespiertoindex.4a8ea5192770a42e9892.js
@@ -0,0 +1,167 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/img/hotel.jpg":
+/*!***************************!*\
+  !*** ./src/img/hotel.jpg ***!
+  \***************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (__webpack_require__.p + "6ccbd79daeed26b25a96931b004887ab.jpg");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/global */
+/******/ 	(() => {
+/******/ 		__webpack_require__.g = (function() {
+/******/ 			if (typeof globalThis === 'object') return globalThis;
+/******/ 			try {
+/******/ 				return this || new Function('return this')();
+/******/ 			} catch (e) {
+/******/ 				if (typeof window === 'object') return window;
+/******/ 			}
+/******/ 		})();
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/publicPath */
+/******/ 	(() => {
+/******/ 		var scriptUrl;
+/******/ 		if (__webpack_require__.g.importScripts) scriptUrl = __webpack_require__.g.location + "";
+/******/ 		var document = __webpack_require__.g.document;
+/******/ 		if (!scriptUrl && document) {
+/******/ 			if (document.currentScript)
+/******/ 				scriptUrl = document.currentScript.src
+/******/ 			if (!scriptUrl) {
+/******/ 				var scripts = document.getElementsByTagName("script");
+/******/ 				if(scripts.length) scriptUrl = scripts[scripts.length - 1].src
+/******/ 			}
+/******/ 		}
+/******/ 		// When supporting browsers where an automatic publicPath is not supported you must specify an output.publicPath manually via configuration
+/******/ 		// or pass an empty string ("") and set the __webpack_public_path__ variable from your code to use your own logic.
+/******/ 		if (!scriptUrl) throw new Error("Automatic publicPath is not supported in this browser");
+/******/ 		scriptUrl = scriptUrl.replace(/#.*$/, "").replace(/\?.*$/, "").replace(/\/[^\/]+$/, "/");
+/******/ 		__webpack_require__.p = scriptUrl;
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
+(() => {
+/*!******************************************!*\
+  !*** ./src/necesario/despierto/index.js ***!
+  \******************************************/
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _img_hotel_jpg__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../../img/hotel.jpg */ "./src/img/hotel.jpg");
+
+const progressBar = document.getElementById("progress-bar");
+progressBar.style = 'width: 25%'
+
+const subtitle = document.getElementById("subtitle");
+subtitle.innerHTML = 'En el carro'
+
+const p = document.getElementById("p1");
+p1.innerHTML = `Por fin llegamos, aunque me siento muy cansado, debi dormirme en el
+camino, fueron más de 5 horas de camino. Además ayer me desvele todo la
+noche, estoy que me muero. Pero bueno ya no puedo hacer nada más, pondre
+mi mejor cara y disfrutare del viaje<br><br>
+Hoy puedo... ir a ... tres lugares<br><br>Estoy algo cansado, pero no importa, a que lugar iré primero...`;
+
+
+const img = document.getElementById("img");
+img.src = _img_hotel_jpg__WEBPACK_IMPORTED_MODULE_0__["default"]
+
+
+
+const list = document.getElementById("list");
+
+const li1 = document.createElement('li')
+li1.className = 'list-group-item'
+const a1 = document.createElement('a')
+a1.href = 'cansado.html'
+a1.innerHTML = 'Ir a la playa'
+li1.appendChild(a1)
+list.appendChild(li1)
+ 
+const li2 = document.createElement('li')
+li2.className = 'list-group-item'
+const a2 = document.createElement('a')
+a2.href = 'cansado.html'
+a2.innerHTML = 'Ir a la picina del hotel'
+li2.appendChild(a2)
+list.appendChild(li2)
+ 
+const li3 = document.createElement('li')
+li3.className = 'list-group-item'
+const a3 = document.createElement('a')
+a3.href = 'cansado.html'
+a3.innerHTML = 'Nadar en el rio'
+li3.appendChild(a3)
+list.appendChild(li3)
+
+
+
+
+})();
+
+/******/ })()
+;
